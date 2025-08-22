9224df8cc4949fdc72e6296104add549f7d4d8e0
jackruby45 <timothybickford@gmail.com>
2025-08-21 22:51:32 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 This contains everything you need to run your app locally.
 
-View your app in AI Studio: https://ai.studio/apps/drive/1xcqdzBLXGOppOygq7IKTT-OMOWNxRzF9
+View your app in AI Studio: https://ai.studio/apps/drive/1-WofMbN1PeEWpsVcg1vqGSathceO6WRA
 
 ## Run Locally
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import { process } from 'node:process';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+  // Vercel will set process.env variables during the build process.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    // Vite config
+    define: {
+      // This replaces `process.env.API_KEY` in the source code with the actual
+      // environment variable value at build time.
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    build: {
+      outDir: 'dist'
+    }
+  }
+})
